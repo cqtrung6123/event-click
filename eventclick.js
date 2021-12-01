59ef62098b833ae5d7c494782c9301535fed0ba5
cqtrung6123 <93904342+cqtrung6123@users.noreply.github.com>
2021-12-01 16:33:08 +0700

    Add files via upload

---
+++ eventclick.js
@@ -0,0 +1,12 @@
+var msg='<div class=\"header"><a id=\"close"\ href="#">close X</a></div>';
+msg+='<div><h2>System Maintenance</h2>';
+msg+='Our servers are being update between 3 and 4 a.m';
+msg+='During this time , there may be minor disuptions to service.</div>';
+var elNote=document.createElement('div');
+elNote.setAttribute('id','note');
+elNote.innerHTML.appendChild(elNote);
+function dismissNote(){
+  document.body.removeChild(elNote);
+}
+var elClose=document.getElementById('close');
+elClose.addEventListener('click',dismissNote,false);
